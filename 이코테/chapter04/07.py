a86b477ad4e67b894b656313c0a3a409c67123b5
zio <jiho8345@naver.com>
2021-04-25 00:41:50 +0900

    implementation

---
+++ 이코테/chapter04/07.py
@@ -0,0 +1,24 @@
+#럭키 스트레이트
+n = input()
+
+half = int(len(n)/2)
+
+num = int(n)
+
+left=0
+right=0
+
+
+for i in range(len(n)):
+    mol = num%10
+    num = int(num/10)
+    if i < half:
+        right += mol
+    else:
+        left += mol
+
+if(left == right):
+    print("LUCKY")
+else:
+    print("READY")
+
